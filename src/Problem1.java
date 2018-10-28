ded81ede05f8ab5be76ba68993db51cda34af3e0
Jake Hernandez <jakejhdz@yahoo.com>
2018-10-28 00:15:10 +0000

    commented it epic style
    made it epic

--- src/Problem1.java
+++ src/Problem1.java
@@ -2,13 +2,15 @@
 public class Problem1 {
 	
 	public static void main(String[] args) {
-		
+		# summing all the numbers 
 		int sum = 0;
 		
 		for(int i = 0; i < 1000; i++)
 			if(i % 5 == 0 || i % 3 == 0)
 				sum += i;
 		
+		# print le sum
+		# this counts as a pull request rite
 		System.out.println("Answer: " + sum);
 	}
 
